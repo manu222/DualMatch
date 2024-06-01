5dd9c89a2a083a1803f9bd01985bcbf899fc3cca
Manuel Araujo Baño <manuaraba@gmail.com>
2024-06-01 22:40:54 +0200

    Flex para la imagen de main

--- lib/main.dart
+++ lib/main.dart
@@ -39,115 +39,128 @@ class VistaInicial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Usuario>? usuarios = Provider.of<UserManager>(context).usuarios;
-    Usuario? user = Provider.of<UserManager>(context).currentUser;
-
-    if (user == null) {
-      print('No hay usuario, estamos en main.dart');
-    } else {
-      print('Usuario: ${user.nombre}' + ' estamos en main.dart');
-    }
-
-    if (usuarios == null) {
-      print('No hay usuarios, estamos en main.dart');
-    } else {
-      print('Usuarios: ${usuarios.length}' + ' estamos en main.dart');
-      usuarios.forEach((user) {
-        print('Usuario: ${user.nombre}' + ' estamos en main.dart');
-      });
-    }
-
     return Scaffold(
       backgroundColor: Colors.pink[100],
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/image2.png'),
-                  fit: BoxFit.cover,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: IntrinsicHeight(
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/image2.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              '¡Bienvenido!',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Login(),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  elevation: 15,
+                                  backgroundColor: Colors.pink,
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                  ),
+                                  shape: const StadiumBorder(
+                                    side: BorderSide(
+                                      color: Colors.black,
+                                      width: 1.5,
+                                    ),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Iniciar Sesión',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => RegisterScreen(),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  elevation: 15,
+                                  backgroundColor: Colors.pink,
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                  ),
+                                  shape: const StadiumBorder(
+                                    side: BorderSide(
+                                      color: Colors.black,
+                                      width: 1.5,
+                                    ),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Registrarse',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            const Text(
+                              '¡Citas dobles para el doble de diversión!',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    '¡Bienvenido!',
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Login(),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        elevation: 15,
-                        backgroundColor: Colors.pink,
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        shape: const StadiumBorder(
-                          side: BorderSide(color: Colors.black, width: 1.5),
-                        ),
-                      ),
-                      child: const Text(
-                        'Iniciar Sesión',
-                        style: TextStyle(fontSize: 18, color: Colors.black),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterScreen(),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        elevation: 15,
-                        backgroundColor: Colors.pink,
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        shape: const StadiumBorder(
-                          side: BorderSide(color: Colors.black, width: 1.5),
-                        ),
-                      ),
-                      child: const Text(
-                        'Registrarse',
-                        style: TextStyle(fontSize: 18, color: Colors.black),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    '¡Citas dobles para el doble de diversión!',
-                    style: TextStyle(fontSize: 12, color: Colors.black),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
